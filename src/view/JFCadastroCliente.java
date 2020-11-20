3a264dad8aa67c1750ed03eed70943aeb174b1c0
Maria Heloísa Gervazio Lopes <heloisagervazio@gmail.com>
2020-11-20 20:41:58 -0300

    todos arquivos

---
+++ src/view/JFCadastroCliente.java
@@ -0,0 +1,140 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.bean.Cliente;
+import model.bean.Filme;
+import model.dao.ClienteDAO;
+import model.dao.FilmeDAO;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+
+public class JFCadastroCliente extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtNome;
+	private JTextField txtEmail;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFCadastroCliente frame = new JFCadastroCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFCadastroCliente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(5, 5, 87, -1);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Cadastro de Clientes");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(111, 22, 238, 30);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nome Completo:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(24, 58, 115, 30);
+		contentPane.add(lblNewLabel_2);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(137, 63, 231, 24);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Email:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_3.setBounds(91, 98, 38, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(137, 98, 157, 17);
+		contentPane.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Sexo:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_4.setBounds(58, 136, 46, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JRadioButton rdbtnFeminino = new JRadioButton("Feminino");
+		rdbtnFeminino.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdbtnFeminino.setBounds(68, 151, 83, 23);
+		contentPane.add(rdbtnFeminino);
+		
+		JRadioButton rdbtnMasculino = new JRadioButton("Masculino");
+		rdbtnMasculino.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdbtnMasculino.setBounds(66, 177, 109, 23);
+		contentPane.add(rdbtnMasculino);
+		
+		ButtonGroup sexo = new ButtonGroup();
+		sexo.add(rdbtnFeminino);
+		sexo.add(rdbtnMasculino);
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLimpar.setBounds(174, 227, 89, 23);
+		contentPane.add(btnLimpar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnCancelar.setBounds(273, 227, 89, 23);
+		contentPane.add(btnCancelar);
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Cliente c = new Cliente();
+				ClienteDAO dao = new ClienteDAO();
+				c.setNome(txtNome.getText());
+				c.setCpf(txtEmail.getText());
+				
+				if(rdbtnFeminino.isSelected()) {
+					c.setSexo(false);
+				}else if (rdbtnMasculino.isSelected()) {
+					c.setSexo(true);
+				}
+				
+				dao.create(c);
+			}
+		});
+		btnCadastrar.setBounds(58, 227, 105, 23);
+		contentPane.add(btnCadastrar);
+	   
+	}
+}
---
+++ src/view/JFCadastroFilme.java
@@ -0,0 +1,177 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.bean.Filme;
+import model.dao.FilmeDAO;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.JSpinner;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JFCadastroFilme extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtTitulo;
+	private JTextField txtSinopse;
+	private JTextField txtCategoria;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFCadastroFilme frame = new JFCadastroFilme();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFCadastroFilme() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 517, 370);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel JLtitulo = new JLabel(" Cadastrar Filme");
+		JLtitulo.setBounds(151, 15, 212, 21);
+		JLtitulo.setFont(new Font("Tahoma", Font.BOLD, 20));
+		contentPane.add(JLtitulo);
+
+		JLabel JLtitulofilme = new JLabel("T\u00EDtulo");
+		JLtitulofilme.setBounds(5, 44, 46, 19);
+		JLtitulofilme.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(JLtitulofilme);
+
+		txtTitulo = new JTextField();
+		txtTitulo.setBounds(67, 45, 402, 20);
+		contentPane.add(txtTitulo);
+		txtTitulo.setColumns(10);
+
+		JLabel JLsinopse = new JLabel("Sinopse");
+		JLsinopse.setBounds(5, 86, 56, 19);
+		JLsinopse.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(JLsinopse);
+
+		txtSinopse = new JTextField();
+		txtSinopse.setBounds(69, 86, 350, 65);
+		contentPane.add(txtSinopse);
+		txtSinopse.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Categoria");
+		lblNewLabel.setBounds(5, 158, 80, 19);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblNewLabel);
+
+		txtCategoria = new JTextField();
+		txtCategoria.setBounds(91, 162, 318, 20);
+		contentPane.add(txtCategoria);
+		txtCategoria.setColumns(10);
+
+		JSpinner spTempo = new JSpinner();
+		spTempo.setBounds(25, 222, 46, 49);
+		contentPane.add(spTempo);
+
+		JLabel lblNewLabel_1 = new JLabel("Tempo");
+		lblNewLabel_1.setBounds(15, 197, 47, 19);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Imagem");
+		lblNewLabel_2.setBounds(110, 197, 56, 19);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblNewLabel_2);
+
+		JRadioButton rdbtn2d = new JRadioButton("2D");
+		rdbtn2d.setBounds(110, 222, 43, 21);
+		rdbtn2d.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(rdbtn2d);
+
+		JRadioButton rdbtn3d = new JRadioButton("3D");
+		rdbtn3d.setBounds(110, 242, 43, 21);
+		rdbtn3d.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(rdbtn3d);
+
+		ButtonGroup imagem = new ButtonGroup();
+		imagem.add(rdbtn2d);
+		imagem.add(rdbtn3d);
+
+		JLabel lblNewLabel_3 = new JLabel("Audio");
+		lblNewLabel_3.setBounds(221, 197, 35, 19);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(lblNewLabel_3);
+
+		JRadioButton rdbtnDublado = new JRadioButton("Dublado");
+		rdbtnDublado.setBounds(221, 222, 77, 21);
+		rdbtnDublado.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(rdbtnDublado);
+
+		JRadioButton rdbtnLegendado = new JRadioButton("Legendado");
+		rdbtnLegendado.setBounds(221, 242, 97, 21);
+		rdbtnLegendado.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		contentPane.add(rdbtnLegendado);
+
+		ButtonGroup audio = new ButtonGroup();
+		audio.add(rdbtnDublado);
+		audio.add(rdbtnLegendado);
+
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Filme f = new Filme();
+				FilmeDAO dao = new FilmeDAO();
+				f.setTitulo(txtTitulo.getText());
+				f.setSinopse(txtSinopse.getText());
+				f.setCategoria(txtCategoria.getText());
+				f.setTempo(Integer.parseInt(spTempo.getValue().toString()));
+				if (rdbtn2d.isSelected()) {
+					f.setImagem3d(false);
+				} else if (rdbtn3d.isSelected()) {
+					f.setImagem3d(true);
+				}
+				if (rdbtnDublado.isSelected()) {
+					f.setDublado(true);
+				} else if (rdbtnLegendado.isSelected()) {
+					f.setDublado(false);
+				}
+				dao.create(f);
+				
+			}
+		});
+		btnCadastrar.setBounds(122, 295, 97, 23);
+		contentPane.add(btnCadastrar);
+
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnLimpar.setBounds(221, 295, 80, 23);
+		contentPane.add(btnLimpar);
+
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnCancelar.setBounds(303, 295, 97, 23);
+		contentPane.add(btnCancelar);
+	}
+}
